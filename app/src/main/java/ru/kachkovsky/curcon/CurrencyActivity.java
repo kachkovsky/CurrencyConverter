c9aa2d950e664fcfda6861b5c739501c9941bfb4
Igor Kachkovsky <kachkovsky-noreply-spam@yandex.ru>
2017-09-07 02:06:38 +0400

    Initial commit

---
+++ app/src/main/java/ru/kachkovsky/curcon/CurrencyActivity.java
@@ -0,0 +1,13 @@
+package ru.kachkovsky.curcon;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CurrencyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_currency);
+    }
+}
